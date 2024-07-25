ba8a45fcaecee15ea6543a615c2f39431dbce5a9
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-07-25 08:55:34 -0600

    [ADD] user repository

---
+++ src/domain/repositories/UserRepository.ts
@@ -0,0 +1,7 @@
+import { User } from "../entities/User";
+
+export interface UserRepository {
+    findById(id: string): Promise<User | undefined>;
+    findByEmail(email: string): Promise<User | undefined>;
+    save(user: User): Promise<void>;
+}
