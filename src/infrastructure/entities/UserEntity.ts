08ed7c281bfb15a6c2623a0b95a16efed4cce29a
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-07-25 10:18:24 -0600

    [ADD] User entity to ORM

---
+++ src/infrastructure/entities/UserEntity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string; // Hashed password
+
+  constructor(id: string, name: string, email: string, password: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
