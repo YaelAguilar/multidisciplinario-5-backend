2ab295f3be615ed3fb0625005cf4782bd1055769
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-07-25 08:35:58 -0600

    [ADD] main function

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { config as dotenvConfig } from 'dotenv';
+import bootstrap from './src/index';
 
 const app = express();
 dotenvConfig();
-
 const PORT = process.env.PORT;
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    bootstrap();
+});
