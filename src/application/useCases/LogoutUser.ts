fc63591b82b4fc2b017deb63e107844545f443ae
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-07-25 09:51:23 -0600

    [ADD] Logout user use case

---
+++ src/application/useCases/LogoutUser.ts
@@ -0,0 +1,7 @@
+export class LogoutUser {
+    async execute(): Promise<{ success: boolean; message: string }> {
+      // despues veo como hago esta logica, o si pueden haganla, porfavor
+      return { success: true, message: 'Logout successful' };
+    }
+  }
+  
