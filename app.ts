8852efe72c1ddc0ce1cf1f7c82df59a439338752
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-08-01 13:32:26 -0600

    [UPDATE] name of user route

--- app.ts
+++ app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import { config as dotenvConfig } from 'dotenv';
 import bootstrap from './src/index';
 
 dotenvConfig();
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 const PORT = process.env.PORT || 3010;
 
 app.listen(PORT, () => {
