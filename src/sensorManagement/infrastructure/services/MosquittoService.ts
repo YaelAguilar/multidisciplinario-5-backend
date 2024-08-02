4ec43a66d26664dc49ce36d4bb3be5d397aa6672
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-08-01 20:54:56 -0600

    [ADD] webhook

--- src/sensorManagement/infrastructure/services/MosquittoService.ts
+++ src/sensorManagement/infrastructure/services/MosquittoService.ts
@@ -2,6 +2,7 @@ import mqtt, { MqttClient } from 'mqtt';
 import { SensorRepository } from '../../domain/repositories/SensorRepository';
 import { Sensor } from '../../domain/entities/Sensor';
 import { Server as SocketIOServer } from 'socket.io';
+import axios from 'axios';
 
 export class MosquittoService {
   private client: MqttClient;
@@ -42,8 +43,25 @@ export class MosquittoService {
 
       // Emitir datos a través de Socket.IO
       this.io.emit('sensor-data', sensor);
+
+      // Verificar si la distancia es igual o menor a 19 y enviar webhook
+      if (sensor.type === 'distancia' && parseFloat(sensor.value) <= 19) {
+        await this.sendWebhook('Poca comida, alimentando a su hamster');
+      }
     } catch (error) {
       console.error('Error saving sensor data:', error);
+    }
+  }
+
+  private async sendWebhook(message: string): Promise<void> {
+    const webhookUrl = 'https://discord.com/api/webhooks/1268753282411528253/I5bO1LpigWGnnClrDSnhbwmU8z1bFhHszvt6Pr1mH84ZstyRwHuHWv1VB7BIBmJmIk1D';
+    try {
+      await axios.post(webhookUrl, {
+        content: message
+      });
+      console.log('Mensaje enviado a Discord');
+    } catch (error) {
+      console.error('Error al enviar mensaje a Discord:', error);
     }
   }
 }
